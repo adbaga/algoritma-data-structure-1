dfe4c1ba9a49f7fd11139206dd6cfa6918a34fa4
Aditya Bagaskara <adbaga@taltech.ee>
2021-03-19 16:49:03 +0200

    order sheep and goat

--- a1/src/Sheep.java
+++ a1/src/Sheep.java
@@ -1,14 +1,73 @@
-
 public class Sheep {
 
    enum Animal {sheep, goat};
    
    public static void main (String[] param) {
       // for debugging
+      Animal[] farm = new Sheep.Animal [10];
+
+      for (int i=0; i < farm.length; i++) {
+         double rnd = Math.random();
+         if (rnd < 1./2.) {
+            farm[i] = Animal.sheep;
+
+         } else {
+            farm[i] = Animal.goat;
+
+         }
+      }
+
+      System.out.println(java.util.Arrays.toString(farm));
+      Sheep.reorder (farm);
+      System.out.println(java.util.Arrays.toString(farm));
+
+
    }
    
    public static void reorder (Animal[] animals) {
       // TODO!!! Your program here
-   }
-}
+
+
+
+      int f = 0; //f for first
+      int l = animals.length - 1; //l for last
+
+
+      for(int i=0; i <= l; i++){
+
+         if(f<l){
+
+            if(animals[i] == Animal.goat){
+               Animal tmp = animals[i];
+               animals[i] = animals[f];
+               animals[f] = tmp;
+
+               f++; //increment f so we have new beginning
+
+            }
+
+            else  {
+
+               Animal tmp = animals[i];
+               animals[i] = animals[l];
+               animals[l] = tmp;
+
+               l--; //let's decrement l so we have new ending
+               i--; //in case that the last element we swap was sheep
+
+            }
+
+
+
+
+         }
+
+      }
+
+
+
+
+         }
+      }
+
 
